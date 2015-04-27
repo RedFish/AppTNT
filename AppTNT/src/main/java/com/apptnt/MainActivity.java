62e0d8d8b387bbc46f0b696ac78c2eb8aeb36a9d
Richard Guerci <richardg54@gmail.com>
2015-04-27 21:54:42 +0200

    Add authors comments

--- AppTNT/src/main/java/com/apptnt/MainActivity.java
+++ AppTNT/src/main/java/com/apptnt/MainActivity.java
@@ -3,6 +3,11 @@ package com.apptnt;
 import org.apache.cordova.*;
 import android.os.Bundle;
 
+/**
+ * Main Activity initializing PhoneGap (Cordova)
+ * @author Richard GUERCI & Julien SIERRA
+ *
+ */
 
 public class MainActivity extends DroidGap {
 
